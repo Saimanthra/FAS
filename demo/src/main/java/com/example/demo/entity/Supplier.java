d4de081958d5c5c962d38f411cf773ee89f8af60
Saimanthra <86704529+Saimanthra@users.noreply.github.com>
2022-09-26 14:34:42 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/entity/Supplier.java
@@ -0,0 +1,57 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="Supplier")
+public class Supplier {
+	@Id
+	@GeneratedValue
+	
+	private int supplierId;
+	@Column(name = "suppliername",nullable=false)
+	private String supplierName;
+	@Column(name = "Address",nullable=false)
+	private String Address;
+	@Column(name = "phno",nullable=false)
+	private String phno;
+	public int getSupplierId() {
+		return supplierId;
+	}
+	public void setSupplierId(int supplierId) {
+		this.supplierId = supplierId;
+	}
+	public String getSupplierName() {
+		return supplierName;
+	}
+	public void setSupplierName(String supplierName) {
+		this.supplierName = supplierName;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+	
+	
+	
+	
+
+}
